c9f2d7b89d995d6e4d9ca46feef5f27a07d3fca3
dam1 - Varela Parada <A23AlejandroVP@sanclemente.local>
2023-12-04 13:53:43 +0100

    Creados errores

--- edad.js
+++ edad.js
@@ -14,7 +14,7 @@ if (isNaN(edad)) {
     categoria = "adolescente";
     break;
     case edad >= 19 && edad <= 30:
-    categoria = "joven";
+    categoria = "joven"
     break;
     case edad >= 31 && edad <= 64:
     categoria = "adulto";
@@ -25,7 +25,7 @@ if (isNaN(edad)) {
     default:
     alert("Idade fora do rango permitido.");
 }
-if (categoria) {
+if (categoria) 
 alert("Eres un " + categoria + ".");
 }
 }
